393c46ee8594d69226b2144ab870b4e56ea288b3
ricardopnunes <ricardopontin145@gmail.com>
2022-05-03 21:56:21 -0300

    Separa Vetores

--- calcula.php
+++ calcula.php
@@ -1,54 +1,69 @@
 <?php
 $array = [1,2,3,45,6,8,9,78,10,55,33];
+class ClassFor{
 
-function for_biru($array){
-    $cont_par = 0;
-
-    for($i=0;$cont_par<5;$i++){
-        $resto=$array[$i]%2;
-        if($resto == 0){
-            $cont_par++;
-        }
-    }
-    echo "<br><b>Contador de pares for: {$cont_par} <br> Contador i for:{$i}";
-}
-
-function while_biru($array){
-    $i=0;
-    $cont_par=0;
-    while($cont_par<5){
-        $resto=$array[$i]%2;
-
-        if($resto == 0){
-          $cont_par++;
-        }
-       $i++;
-    }
-
-    echo "<br>Contador de pares while: {$cont_par} <br> Contador i while:{$i}";
-
-}
-
-function doo_while($array)
-{
-    $i=0;
-    $cont_par = 0;
-    do{
-        $resto=$array[$i]%2;
-        if($resto == 0){
-            $cont_par++;
-        }
-       $i++;
-    } while($cont_par<5);
+    public $pares = [];
+    public $impares = [];
     
-    echo "<br>Contador de pares do_while: {$cont_par} <br> Contador i do_while: {$i}</b>";
-}
+    public function separa_vetores($array){
 
-echo "Data Hora Inicio: " .date("d/m/y ") .date(" g:i:s a");
-for_biru($array);
-echo '<br><br><br>';
-while_biru($array);
-echo '<br><br><br>';
-doo_while($array);
-echo "<br>Data Hora Final: " .date("d/m/y ") .date(" g:i:s a");
+        $cont_par = 0;
+        $cont_impares = 0;
 
+        for($i=0; $i < count($array); $i++){
+
+            $resto=$array[$i]%2;
+        
+            if($resto == 0){
+
+                $pares[$cont_par] = $array[$i];
+                $cont_par++;
+            }
+
+            else{
+
+                $impares[$cont_impares] = $array[$i];
+                $cont_impares++;
+            }
+        }
+        
+        echo "Numeros de pares: {$cont_par}<br> Numero de Impares: {$cont_impares} <br>";
+        print_r($pares);
+        echo "<br>";
+        print_r($impares);
+        return $impares;
+    }
+
+
+    public function conta_impares($impares){
+        $cont_impares =0;
+        for($i=0;$i<count($impares); $i++){
+            $resto = $impares[$i] % 2;
+            if($resto != 0){
+                $cont_impares++;
+            }
+        }
+        
+        echo "<br> {$cont_impares} <br>";
+        var_dump($impares);
+    }
+
+    public function doo_while($array)
+    {
+        $i=0;
+        $cont_par = 0;
+        do{
+            $resto=$array[$i]%2;
+            if($resto == 0){
+                $cont_par++;
+            }
+        $i++;
+        } while($cont_par<5);
+    
+        echo "<br>Contador de pares Classe do_while: {$cont_par} <br> Contador i do_while: {$i}</b>";
+    }
+}  
+
+$n = new ClassFor;
+$n->separa_vetores($array);
+$n->conta_impares($impares);
