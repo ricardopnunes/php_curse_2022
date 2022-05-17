b63acfcc16a774f0eedc5d9964b39cf534dfb801
ricardopnunes <ricardopontin@gmail.com>
2022-05-17 14:19:51 -0300

    Resolvendo Primos e perfeitos

--- cabecalho.php
+++ cabecalho.php
@@ -1,4 +1,36 @@
-<form action="usuario.php" method="post">
-    Numero:  <input type="number" name="num" /><br />
-    <input type="submit" name="submit" value="Salvar" />
-</form>
+<?php
+
+$primos = [2];
+$contPrimos = 1;
+
+$numero = 2;
+$divisor = 2;
+
+while ($contPrimos < 5) {
+
+    $contDiv = 0;
+    $numero++;
+
+    for ($divisor = 2; $divisor <= $numero; $divisor++) {
+        $resto = $numero % $divisor;
+
+        if ($resto == 0 && $divisor <= $numero) {
+            $contDiv++;
+        }
+
+        if ($contDiv >= 2) {
+            break;
+        }
+    }
+
+    if ($contDiv != 1) {
+        continue;
+    }
+
+    array_push($primos, $numero);
+    $contPrimos++;
+
+}
+
+$primosSTR = implode(", ", $primos);
+echo "Os 5 primos sao: " . $primosSTR;
--- usuario.php
+++ usuario.php
@@ -1,64 +1,88 @@
 <?php
 
-$numero = [2,3,4,5,6,9,11,13,17,19,23,28,29,31,37,43,47,51,496];
-$perf = [1,2,3,6,28,496];
-$i =0;
-$primos = [];
-$perfeitos = [];
-$contPrimos = 0;
-$contPerfeitos =0;
+$primo_array = [2,3,4,5,6,9,11,13,17,19,23,28,29,31,37,43,47,51,496];
+$perf_array = [1,2,3,6,28,500,31,29,18,496,5,9];
 
+function separa_primos($primo_array)
+{
+    $i = 0;
+    $cont_primos = 0;
+    $primos = [];
+    $div = 2;
 
-while($contPrimos < 5) {
-
-    $div = 0;
-    $n = $numero[$i];
-
-    for($j=1; $j <= $n; $j++) {
-
-        $resto = $n % $j;
+    for($i=0; $cont_primos < 5; $i++)
+    {
+        $conta_div=0;
+        $n = $primo_array[$i];
     
-
-        if($resto == 0){
-            $div++;
+        for($div=2; $div <= $n; $div++)
+        {
+            $resto = $n % $div;
+    
+            if($resto == 0 && $div <= $n)
+            {
+                $conta_div++;
+            }
+    
+            if($conta_div >=2)
+            {
+                break;
+            }
         }
-    }
+        
+        if($conta_div != 1)
+        {
+            continue;
+        }
     
-    if($div == 2){
-
-        $contPrimos++;
         array_push($primos, $n);
-    }   
-    
-    $i++;
-}
-
-for($j=0; $j < count($primos); $j++){
-    echo " Primo: " . $primos[$j];
-}
-
-
-$i =0;
-while($contPerfeitos < 3) {
-    $soma =0;
-    $n = $numero[$i];
-
-    for($j=1; $j <= $n; $j++) {
-
-        $resto = $n % $j;
-
-        if($resto == 0 && $j != $n){
-            $soma+=$j;
-            echo $soma;
-        }
+        $cont_primos++;
     }
-       
-
     
-    $contPerfeitos++;
-    $i++;
+    for($j=0; $j < count($primos); $j++)
+    {
+        echo " Primo: " . $primos[$j];
+    }
 }
 
-for($j=0; $j < count($numero); $j++){
-    echo " Perfeito: " . $numero[$j];
+function separa_perfeitos($perf_array)
+{
+    $cont_perfeitos =0;
+    $perfeitos = [];
+    $i =0;
+
+    while($cont_perfeitos < 3) 
+    {
+        $soma =0;
+        $n = $perf_array[$i];
+        $metade = $n/2;
+
+        for($j=1; $j <= $metade; $j++) 
+        {
+            $resto = $n % $j;
+
+            if($resto == 0)
+            {
+                $soma+=$j;
+            }
+        }
+    
+        if($soma == $n)
+        {
+            array_push($perfeitos, $n);;
+            $cont_perfeitos++;
+        }
+
+        $i++;
+    }
+
+    for($i=0; $i < count($perfeitos); $i++)
+    {
+        echo " Perfeito: " . $perfeitos[$i];
+    }
 }
+ 
+separa_primos($primo_array);
+echo "\n";
+separa_perfeitos($perf_array);
+
