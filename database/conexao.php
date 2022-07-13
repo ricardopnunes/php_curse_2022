f9ae0d397ad30e19e4d10d0ddacb00298a9c00db
ricardopnunes <ricardopontin145@gmail.com>
2022-07-12 21:54:18 -0300

    Criando os controllers e ajustando Models

--- database/conexao.php
+++ database/conexao.php
@@ -1,6 +1,6 @@
 <?php
 
-require "../controllers/PessoaControllers.php";
+//require "../controllers/PessoaControllers.php";
 
 $host = "localhost";
 $user = "aluno";
@@ -13,34 +13,30 @@ if($conexao->connect_error)
 {
     die("Conexão com o banco de dados falhou: ". $conexao->connect_error);
 }
-else
-{
-    echo "Deu bom!\n";
-}
 
 
 
-foreach($dados_pessoas as $dado){
-    $sql = "INSERT INTO curso22.pessoas (nome,email,cpf,rg,genero,rede_social,status,phone,tipo,cep) 
-    VALUES (
-    '{$dado['nome']}',
-    '{$dado['email']}',
-    '{$dado['cpf']}',
-    '{$dado['rg']}',
-    '{$dado['gender']}',
-    '{$dado['redeSocial']}',
-    '{$dado['status']}',
-    '{$dado['phone']}',
-    '{$dado['type']}',
-    '{$dado['cep']}')";
+// foreach($dados_pessoas as $dado){
+//     $sql = "INSERT INTO curso22.pessoas (nome,email,cpf,rg,genero,rede_social,status,phone,tipo,cep) 
+//     VALUES (
+//     '{$dado['nome']}',
+//     '{$dado['email']}',
+//     '{$dado['cpf']}',
+//     '{$dado['rg']}',
+//     '{$dado['gender']}',
+//     '{$dado['redeSocial']}',
+//     '{$dado['status']}',
+//     '{$dado['phone']}',
+//     '{$dado['type']}',
+//     '{$dado['cep']}')";
 
-    if ($conexao->query($sql) === TRUE) {
-        echo "Registros adicionados!\n";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conexao->error;
-    }
+//     if ($conexao->query($sql) === TRUE) {
+//         echo "Registros adicionados!\n";
+//     } else {
+//         echo "Error: " . $sql . "<br>" . $conexao->error;
+//     }
 
-}
+// }
 
 
   
